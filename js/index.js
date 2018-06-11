c073a3d5c7de983b3847cd814aa810da38d3e075
krystynaszur <39401289+krystynaszur@users.noreply.github.com>
2018-06-11 08:35:11 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,10 +8,6 @@
 	var chosenLink = this.getAttribute('href');
 	console.log(chosenLink);
 	
-	var modals = document.querySelectorAll('.modal');
-	for(var i = 0; i < modals.length; i++) {
-		modals[i].classList.remove('show');
-	};
 	document.querySelector(chosenLink).classList.add('show');
 	
 }
@@ -24,6 +20,10 @@
 	var hideModal = function(event){
 		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
+		
+		var modals = document.querySelectorAll('.modal');
+		for(var i = 0; i < modals.length; i++) {modals[i].classList.remove('show');
+		};
 	};
 	
 	var closeButtons = document.querySelectorAll('.modal .close');
